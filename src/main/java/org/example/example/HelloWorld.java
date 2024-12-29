acd950f640f21d708ee8503adf916699b26f54c3
SpaceJumper <pavel-laz2010@mail.ru>
2024-12-29 16:00:46 +0300

    Добавлен обработчик с параметром

--- src/main/java/org/example/example/HelloWorld.java
+++ src/main/java/org/example/example/HelloWorld.java
@@ -5,11 +5,17 @@ import io.javalin.Javalin;
 public class HelloWorld {
     public static void main(String[] args) {
         // Создаем приложение
-        var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
-        });
+        var app = Javalin.create(config ->
+            config.bundledPlugins.enableDevLogging()
+        );
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/hello", ctx -> {
+            var param = ctx.queryParam("name");
+            if (param != null)
+                ctx.result("Hello," + param + "!");
+            else
+                ctx.result("Hello, World!");
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
